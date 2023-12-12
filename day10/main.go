1bfd8e0640df997e27007e093c3a02bee99a2c00
Tom Wright <tom.suffern.wright@gmail.com>
2023-12-13 08:12:50 +1100

    feat: day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -16,15 +16,184 @@ func main() {
 
 func part1(content string) {
 	grid := parseGrid(content)
-	path := parseLoop(grid)
+	path := parsePath(grid)
 	fmt.Println("Part 1", len(path)/2)
 }
 
 func part2(content string) {
-	fmt.Println("Part 2", "???")
+
+	grid := parseGrid(content)
+	largeGrid := constructHighResolutionGrid(grid)
+
+	floodfillGrid(largeGrid)
+
+	count := countEnclosed(largeGrid)
+	fmt.Println("Part 2", count, "tiles")
 }
 
-func parseLoop(grid [][]rune) [][]int {
+func countEnclosed(grid [][]rune) int {
+	count := 0
+	for i := 0; i < len(grid); i += 3 {
+		for j := 0; j < len(grid[i]); j += 3 {
+			// combine the cells in this local 3x3
+			cells := string([]rune{
+				grid[i][j],
+				grid[i][j+1],
+				grid[i][j+2],
+				grid[i+1][j],
+				grid[i+1][j+1],
+				grid[i+1][j+2],
+				grid[i+2][j],
+				grid[i+2][j+1],
+				grid[i+2][j+2],
+			})
+			if cells == "........." {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func floodfillGrid(grid [][]rune) {
+	blocked := 'x'
+	filled := 'O'
+	startRow := 0
+	startColumn := 0
+	maxRow := len(grid) - 1
+	maxColumn := len(grid[0]) - 1
+
+	stack := [][2]int{{startRow, startColumn}}
+	pop := func() (int, int) {
+		elem := stack[len(stack)-1]
+		stack = stack[:len(stack)-1] // pop from the end because memory
+		return elem[0], elem[1]
+	}
+	push := func(coords [2]int) {
+		stack = append(stack, coords)
+	}
+
+	for len(stack) > 0 {
+		row, column := pop()
+		grid[row][column] = filled
+
+		directions := [4][2]int{
+			{row - 1, column},
+			{row + 1, column},
+			{row, column - 1},
+			{row, column + 1},
+		}
+
+		for _, next := range directions {
+			nextRow := next[0]
+			nextColumn := next[1]
+			if nextRow < 0 || nextRow > maxRow || nextColumn < 0 || nextColumn > maxColumn {
+				continue
+			}
+			nextCh := grid[nextRow][nextColumn]
+			if nextCh == blocked || nextCh == filled {
+				continue
+			}
+			push(next)
+		}
+	}
+
+}
+
+func constructHighResolutionGrid(grid [][]rune) [][]rune {
+	largeGrid := make([][]rune, len(grid)*3)
+	for i := range largeGrid {
+		largeGrid[i] = make([]rune, len(grid[0])*3)
+		for j := range largeGrid[i] {
+			largeGrid[i][j] = '.'
+		}
+	}
+
+	applyPatch := func(patch [][]rune, row int, column int) {
+		for i, patchRow := range patch {
+			for j, ch := range patchRow {
+				largeGrid[row*3+i][column*3+j] = ch
+			}
+		}
+	}
+
+	path := parsePath(grid)
+
+	for _, coords := range path {
+		row := coords[0]
+		column := coords[1]
+		ch := grid[row][column]
+
+		if ch == '-' {
+			applyPatch([][]rune{
+				{'.', '.', '.'},
+				{'x', 'x', 'x'},
+				{'.', '.', '.'},
+			}, row, column)
+		}
+
+		if ch == '|' {
+			applyPatch([][]rune{
+				{'.', 'x', '.'},
+				{'.', 'x', '.'},
+				{'.', 'x', '.'},
+			}, row, column)
+		}
+
+		if ch == 'F' {
+			applyPatch([][]rune{
+				{'.', '.', '.'},
+				{'.', 'x', 'x'},
+				{'.', 'x', '.'},
+			}, row, column)
+		}
+
+		if ch == '7' {
+			applyPatch([][]rune{
+				{'.', '.', '.'},
+				{'x', 'x', '.'},
+				{'.', 'x', '.'},
+			}, row, column)
+		}
+
+		if ch == 'J' {
+			applyPatch([][]rune{
+				{'.', 'x', '.'},
+				{'x', 'x', '.'},
+				{'.', '.', '.'},
+			}, row, column)
+		}
+
+		if ch == 'L' {
+			applyPatch([][]rune{
+				{'.', 'x', '.'},
+				{'.', 'x', 'x'},
+				{'.', '.', '.'},
+			}, row, column)
+		}
+
+		if ch == 'S' {
+			/*
+			 * Cheating here because we can just look at the input
+			 * to determine what "shape" this should be in 3x3
+			 *
+			 * To do this dynamically:
+			 * - The adjacent points to S are second and second-last elements of path
+			 * - use findCardinalDirection to get direction from S
+			 * - apply a patch based on the two cardinal directions
+			 */
+			applyPatch([][]rune{
+				{'.', 'x', '.'},
+				{'x', 'x', '.'},
+				{'.', '.', '.'},
+			}, row, column)
+		}
+	}
+
+	return largeGrid
+}
+
+func parsePath(grid [][]rune) [][]int {
 	startRow, startColumn := findStart(grid)
 
 	row, column, last := startRow, startColumn, "none"
@@ -108,7 +277,7 @@ func findNext(grid [][]rune, row int, column int, last string) (int, int, string
 	for _, direction := range directionChecks {
 		found, nextRow, nextColumn = direction()
 		if found {
-			last := findLastDirection(row, column, nextRow, nextColumn)
+			last := findCardinalDirection(row, column, nextRow, nextColumn)
 			return nextRow, nextColumn, last
 		}
 	}
@@ -116,20 +285,20 @@ func findNext(grid [][]rune, row int, column int, last string) (int, int, string
 	return -1, -1, "unknown"
 }
 
-func findLastDirection(row int, column int, nextRow int, nextColumn int) string {
-	if row == nextRow && column == nextColumn-1 {
+func findCardinalDirection(toRow int, toColumn int, fromRow int, fromColumn int) string {
+	if toRow == fromRow && toColumn < fromColumn {
 		return "west"
 	}
 
-	if row == nextRow && column == nextColumn+1 {
+	if toRow == fromRow && toColumn > fromColumn {
 		return "east"
 	}
 
-	if row == nextRow-1 && column == nextColumn {
+	if toRow < fromRow && toColumn == fromColumn {
 		return "north"
 	}
 
-	if row == nextRow+1 && column == nextColumn {
+	if toRow > fromRow && toColumn == fromColumn {
 		return "south"
 	}
 
